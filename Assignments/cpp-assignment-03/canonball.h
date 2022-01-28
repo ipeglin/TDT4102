8d910556c55fec0cc43d764afe8cb40f62b4ed1d
ipeglin <philip@eglin.no>
2022-01-28 17:21:21 +0100

    Added further functions to header file. Also created test_deviation function in main.cpp

--- Assignments/cpp-assignment-03/canonball.h
+++ Assignments/cpp-assignment-03/canonball.h
@@ -1,9 +1,26 @@
 #pragma once
+#include <vector>
 
 double acclY();
+
 double velY(double init_velocityY, double time);
+
 double posX(double init_position, double init_velocity, double time);
 double posY(double init_position, double init_velocity, double time);
 
 void print_time(double time);
 double flight_time(double init_velocityY);
+
+double get_user_input_theta();
+double get_user_input_abs_velocity();
+
+double deg_to_rad(double deg);
+
+double get_velX(double theta, double abs_velocity);
+double get_velY(double theta, double abs_velcity);
+
+double get_distance_traveled(double velX, double velY);
+
+double target_practice(double distance_to_target, double velX, double velY);
+
+std::vector<double> get_velocity_vector(double theta, double abs_velocity);
--- Assignments/cpp-assignment-03/main.cpp
+++ Assignments/cpp-assignment-03/main.cpp
@@ -1,7 +1,28 @@
 
 #include <iostream>
+#include <stdlib.h>
+#include <string>
 #include <canonball.h>
 
-int main() {
+using namespace std;
 
+void test_deviation(double compare_operand, double to_operand, double max_error, string name);
+
+int main() {
+    double max_error{0.0001};
+
+    test_deviation(velY(25, 2.5), 0.475, max_error, "velY(25, 2.5)");
+    test_deviation(velY(25, 5.0), -24.05, max_error, "velY(25, 5.0)");
+
+    test_deviation(posX(0, 50, 2.5), 125.0, max_error, "posX(0, 50, 2.5)");
+    test_deviation(posX(0, 50, 5), 250.0, max_error, "posX(0, 50, 5)");
+    
+    test_deviation(posY(0, 25, 2.5), 31.84, max_error, "posY(0, 25, 2.5)");
+    test_deviation(posY(0, 25, 5), 2.375, max_error, "posY(0, 25, 5)");
+}
+
+
+void test_deviation(double compare_operand, double to_operand, double max_error, string name) {
+    double deviation{abs(to_operand) - abs(compare_operand)};
+    cout << ((deviation > max_error) ? "Ikke korrekt" : name + " = " + to_string(to_operand)) << endl;
 }
