55728653e5602904ff400932e07b2e7e66a2caf8
ipeglin <philip@eglin.no>
2022-02-21 10:31:32 +0100

    Removed unnecessary code from main function. Also removed 'using namespace std'

--- Assignments/cpp-assignment-06/main.cpp
+++ Assignments/cpp-assignment-06/main.cpp
@@ -4,27 +4,19 @@
 #include "course_catalog.h"
 #include "temp.h"
 
-using namespace std;
-
 int main() {
-    string test_file{"test.txt"};
 
-    // vector<string> word_vector{get_words_from_user()};
-    // write_words_to_file(word_vector, test_file);
-    // rewrite_to_new_file(test_file, "new.txt");
-
-    // std::map<char, int> character_map{get_character_map("grunnlov.txt")};
-    // show_character_map(character_map);
-
+    // 2b
     // The [] operator is not supported for maps. Only arrays and vectors
 
     Course_catalog cc;
+    cc.load_catalog();
 
+    // 3d
     // insert() does not overwrite map values, but the [] operator does
 
-    cout << cc;
+    std::cout << "-------- COURSES --------" << std::endl
+              << cc;
 
-    ifstream temp_file{"temperatures.txt"};
-    Temps t;
-    temp_file >> t;
+    
 }
