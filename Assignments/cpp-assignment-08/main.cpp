67361a7b1acf4746d14dd9cfdad9085326c1f14a
ipeglin <philip@eglin.no>
2022-03-14 13:16:55 +0100

    Added comments to 2) in main.cpp

--- Assignments/cpp-assignment-08/main.cpp
+++ Assignments/cpp-assignment-08/main.cpp
@@ -12,13 +12,13 @@ int main() {
 	std::cout << matrix_1;
 	std::cout << std::endl;
 
-    Matrix matrix_2{matrix_1};
+    Matrix matrix_2{matrix_1}; // Test the overloaded = operator
     std::cout << matrix_2 << std::endl;
 
-    Matrix identity_matrix(4);
+    Matrix identity_matrix(4); // Testing the explicit constructor
 	std::cout << identity_matrix << std::endl;
 
-    Matrix matrix_3(identity_matrix);
+    Matrix matrix_3(identity_matrix); // Testing deep copy constructor
     std::cout << matrix_3 << std::endl;
 
     // 3)
