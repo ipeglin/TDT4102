2e52ee146639cb4e4820f8e789790350e413718b
ipeglin <philip@eglin.no>
2022-03-14 12:12:19 +0100

    Added cin for fibonacci length, and added comments to header file

--- Assignments/cpp-assignment-08/fibonacci.cpp
+++ Assignments/cpp-assignment-08/fibonacci.cpp
@@ -19,9 +19,8 @@ void print_array(int* array, int array_length) {
 }
 
 void create_fibonacci() {
-    int sequence_length{10};
-    // std::cout << "How many numbers do you want in the sequence: ";
-    // std::cin >> sequence_length;
+    std::cout << "How many numbers do you want in the sequence: ";
+    std::cin >> sequence_length;
     int *fibonacci_numbers{new int[sequence_length]};
     fill_in_fibonacci_numbers(fibonacci_numbers, sequence_length);
     print_array(fibonacci_numbers, sequence_length);
--- Assignments/cpp-assignment-08/fibonacci.h
+++ Assignments/cpp-assignment-08/fibonacci.h
@@ -1,7 +1,32 @@
 #pragma once
 #include "utilities.h"
 
+/**
+ * @brief Fill an array with a specific amount of numbers in the Fibonacci sequence
+ * 
+ * @param results Pointer to array
+ * @param length Amount of Fibonacci numbers to store
+ */
 void fill_in_fibonacci_numbers(int* results, int length);
+
+/**
+ * @brief Get the fibonacci number by index
+ * 
+ * @param index Index of the fibonacci number
+ * @return int 
+ */
 int get_fibonacci_number_by_index(int index);
+
+/**
+ * @brief Print content of array
+ * 
+ * @param array Array to be displayed
+ * @param array_length Amount of elements in the array
+ */
 void print_array(int* array, int array_length);
+
+/**
+ * @brief Create a fibonacci object
+ * 
+ */
 void create_fibonacci();
