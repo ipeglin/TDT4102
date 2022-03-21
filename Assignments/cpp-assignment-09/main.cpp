a065fe37cb739f6668a6845b19564cdba0a45604
ipeglin <philip@eglin.no>
2022-03-21 17:45:48 +0100

    Added tests for meeting class. There is a memory leak somewhere, but I havent found it as of now. Just pushing to remote to be able to pull on laptop

--- Assignments/cpp-assignment-09/main.cpp
+++ Assignments/cpp-assignment-09/main.cpp
@@ -4,6 +4,7 @@
 #include "meeting.h"
 
 int main() {
+    // Oppgave 1
     Person person_1("Ola Normann", "ola@normann.no");
     
     Car car_1(4);
@@ -12,6 +13,15 @@ int main() {
     std::cout << person_1 << std::endl;
     std::cout << person_2 << std::endl;
 
+
+    // Oppgave 2
     Campus campus{Campus::Trondheim};
     std::cout << campus << std::endl;
+
+
+    // Oppgave 3
+    std::shared_ptr<Person> meeting_leader = std::make_shared<Person>("Ian Philip Eglin", "philip@eglin.no");
+
+    Meeting meeting(1, 12, 14, Campus::Trondheim, "How to pass TDT4102 without a cookbook 101", meeting_leader);
+    std::cout << meeting << std::endl;
 }
