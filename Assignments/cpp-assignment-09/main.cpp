e6ea3f07130e1ceff784fe0282d10b50ba5dd4cd
ipeglin <philip@eglin.no>
2022-03-21 15:47:06 +0100

    Adde test of two Person instances, and printed out their information

---
+++ Assignments/cpp-assignment-09/main.cpp
@@ -0,0 +1,13 @@
+#include "utilities.h"
+#include "person.h"
+#include "car.h"
+
+int main() {
+    Person person_1("Ola Normann", "ola@normann.no");
+    
+    Car car_1(4);
+    Person person_2("Kari Normann", "kari@normann.no", &car_1);
+
+    std::cout << person_1 << std::endl;
+    std::cout << person_2 << std::endl;
+}
