919c411daa770e4e81adc7a94515021e0deb5a2a
ipeglin <philip@eglin.no>
2022-03-21 14:22:47 +0100

    Added utilities file for libraries

---
+++ Assignments/cpp-assignment-09/utilities.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <memory>
+#include <stdio.h>
+#include <cassert>
+#include <utility>
